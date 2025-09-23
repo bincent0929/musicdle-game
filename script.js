33d9c2e2e70f006eda8a0bf49aa5c8b2dcc2ac5f
Vincent Roberson <ghub@varmail.org>
2025-09-23 19:13:43 +0000

    started work on the reset button 	modified:   script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 // this is for restoring the page back to a new game state
 //const gameDiv = document.getElementById('game').innerHTML;
 
+const gameStartHTML = `
+            <h1>test</h1>
+            
+            <div class="reset_button_div">
+                <button id="reset_button">
+                    Do you want to reset the game?
+                </button>
+            </div>
+        `;
+
+const gameResetHTML = `
+            <div class="start_button_div">
+                <button id="start_button">
+                    Do you want to start the game?
+                </button>
+            </div>
+        `;
+
 /**
  * the document.addEventListener('DOMContentLoaded', function() {});
  * is for making sure that the page is loaded and that the elements
@@ -15,8 +33,15 @@ document.addEventListener('DOMContentLoaded', function() {
     
     function startGame() {
         const game = document.getElementById('game');
-        game.innerHTML = `
-            <h1>test</h1>
-        `;
+        game.innerHTML = gameStartHTML;
+    }
+
+    document.getElementById('reset_button').addEventListener('click', function() {
+        resetGame();
+    });
+
+    function resetGame() {
+        const game = document.getElementById('game');
+        game.innerHTML = gameResetHTML;
     }
 });
