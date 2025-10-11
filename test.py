2d9bf60085f583115a9283c1cd3c9854337bc7f9
Vincent Roberson <ghub@varmail.org>
2025-10-11 20:29:24 +0000

    got the file name and path fetching implemented 	modified:   test.py

--- test.py
+++ test.py
@@ -4,10 +4,21 @@ def get_file_names(folder_path):
     try:
         items = os.listdir(folder_path)
 
+        files = {}
         # this returns a list with the file name and file path
-        files = [os.path.join(folder_path, item) for item in items 
-                if os.path.isfile(os.path.join(folder_path, item))]
         
+        for item in items:
+            full_path = os.path.join(folder_path, item)
+            if os.path.isfile(full_path):
+                clean_name = item
+
+                if clean_name.lower().endswith('.mp3'):
+                    clean_name = clean_name[:-4]
+
+                clean_name = clean_name.replace('-', ' ')
+
+                files[clean_name] = full_path
+
         print(files)
 
         return files
