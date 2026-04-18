5652cfb0fcafe9fa662181a99421789e05184384
Vincent Roberson <ghub@varmail.org>
2026-04-18 20:14:10 +0000

    updated the api to use the deployment

--- scripts/api.ts
+++ scripts/api.ts
@@ -8,7 +8,7 @@ export async function daily_url_fetch(): Promise<string> {
   try {
     // we need to add something here to have it call the local backend instead
     // of the cloud backend
-    const response = await fetch('http://localhost:3000/api/daily-song-url');
+    const response = await fetch('https://backend.musicdle.xyz/api/daily-song-url');
     if (!response.ok) {
       throw new Error(`Server error: ${response.status}`);
     }
