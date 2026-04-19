bc3164691093891195f8f9a3bca9239ed9f25047
Vincent Roberson <ghub@varmail.org>
2026-04-19 00:45:47 +0000

    changed the run command to not delete the Caddy data

--- docker/docker-start.sh
+++ docker/docker-start.sh
@@ -5,4 +5,4 @@ cd musicdle-game/
 docker build -f docker/Dockerfile -t musicdle .
 docker rm -f musicdle
 # Runs the image in the background under the name musicdle
-docker run -d --name musicdle -p 80:80 -p 443:443 musicdle
+docker run -d --name musicdle -p 80:80 -p 443:443 -v musicdle-caddy-data:/data/caddy musicdle
